64ac353ec52b6b823f61fe53ad1cec7a5b2a4bd4
Naoum Mandrelas <nmandrelas@gmail.com>
2019-12-27 13:25:30 +0200

    --genesis

---
+++ DietMap.Client/Classes/Appointments.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace DietMap.Client.Classes
+{
+
+    public class AppointmentList
+    {
+        public List<Appointment> AppointmentsList { get; set; }
+    }
+
+    public class Appointment
+    {
+        public List<string> Notes { get; set; }
+        public DateTime Date { get; set; }
+        public int AppointmentNumber { get; set; }
+        public Measurements Measurements { get; set; }
+
+    }
+    public class Measurements
+    {
+        public decimal Weight { get; set; }
+        public decimal BMI { get; set; }
+        public decimal BodyFat { get; set; }
+        public double BMR { get; set; }
+        public decimal BodyWater { get; set; }
+        public decimal MuscleMass { get; set; }
+        public int MetabolicAge { get; set; }
+        public decimal VisceralFat { get; set; }
+        public decimal BoneMass { get; set; }
+        public int MuscleQuality { get; set; }
+        public int PhysiqueRating { get; set; }
+    }
+}
