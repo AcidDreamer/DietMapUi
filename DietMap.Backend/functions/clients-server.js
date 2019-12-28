566d767d9d59b417407671a5865ca6a4c6dee471
Naoum Mandrelas <nmandrelas@gmail.com>
2019-12-28 22:09:06 +0200

    -- Backend converted to async from thenables

--- DietMap.Backend/functions/clients-server.js
+++ DietMap.Backend/functions/clients-server.js
@@ -1,70 +1,52 @@
+// Required
 const cors = require('cors');
 const express = require('express');
 const uuid = require('uuid');
+const { Client } = require('./client');
+
+//Static fields
 const clientCollection = 'clients';
 
 function CreateClientServer(_db) {
+
+  //Server setup
   const db = _db;
   const clientsServer = express();
   clientsServer.use(cors({
     origin: true
   }));
 
-  clientsServer.get('/clients', (request, response) => {
-    // var data = [];
-    // let kati = db.collection(clientCollection).get()
-    //   .then((snapshot) => {
-    //     snapshot.forEach((doc) => {
-    //       data.push(doc);
-    //     });
-    //     return data;
-    //   })
-    //   .catch((err) => {
-    //     console.error('Error getting documents', err);
-    //   });
-
+  //GET All clients
+  clientsServer.get('/clients', async (request, response) => {
     const docRef = db.collection(clientCollection);
-    docRef.get()
-      .then(snapshot => {
-        let arrayR = snapshot.docs.map(doc => {
-          return doc.data();
-        });
-        response.json(arrayR);
-        return null;
-      }).catch(function (error) {
-        console.error("got an error", error);
-      })
-
+    let clientsSnapshot = await docRef.get();
+    let clients = clientsSnapshot.docs.map(doc => {return doc.data();})
+    var clientsObj = { "clients" : clients};
+    response.json(clientsObj);
   });
 
-  clientsServer.post('/client', (request, response) => {
+  //Create client
+  clientsServer.post('/client', async (request, response) => {
     let name = request.body["fullname"];
     let newClientId = uuid.v1();
     let docRef = db.collection(clientCollection).doc(newClientId);
-    let setAda = docRef.set({
-      fullname: name,
-      lastVisit: null,
-      createdAt: new Date()
-    });
+    var client = new Client(newClientId, name);
+    await docRef.set(JSON.parse(JSON.stringify(client)));
     response.send(
       'Client with name ' + client.fullname + ' created!'
     );
   });
+
   clientsServer.post('/test', (request, response) => {
     response.send(
       'Test post received!'
     );
   });
-  clientsServer.get('/test', (request, response) => {
+  clientsServer.get('/test',async (request, response) => {
     let newClientId = uuid.v1();
     let docRef = db.collection(clientCollection).doc(newClientId);
-    var client = new Client();
-    let setAda = docRef.set({
-      id : newClientId,
-      fullname: "Test",
-      lastVisit: null,
-      createdAt: new Date()
-    });
+    var client = new Client(newClientId, "TestWithClass");
+    await docRef.set(JSON.parse(JSON.stringify(client)));
     response.send(
       'Client for testing created!'
     );
